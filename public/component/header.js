c2a6f02cca27e9b020b982e1103ddf2835866890
Ozgur GUL <lugrugzo@gmail.com>
2016-11-24 14:01:37 +0300

    header updated

---
+++ public/component/header.js
@@ -0,0 +1,19 @@
+import m from 'mithril'
+
+function controller() {
+
+    var self = this
+}
+
+function view() {
+
+    return (
+        <div className="menu">
+            <ul>
+                <li><a href="/#/">Home</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default { view, controller }
